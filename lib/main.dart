cf44629277299b5bebb2e35be528dcf45968a578
osa989 <osamagamal>
2023-09-03 22:43:28 +0300

    create custom text field

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class NotesApp extends StatelessWidget {
       /*if you want to edit something in theme data 👇 */
       theme: ThemeData(
         fontFamily: "Poppins",
-        brightness: Brightness.light,
+        brightness: Brightness.dark,
         // scaffoldBackgroundColor: Colors.brown[600],
       ),
       home: const NotesView(),
