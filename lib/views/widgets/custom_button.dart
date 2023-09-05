e910106007ed797ec66bc8f8a7abbeb25b8a4368
osa989 <osamagamal>
2023-09-05 15:04:36 +0300

    take input and validate data

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      child: const Center(
-        child: Text(
-          "Add",
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        child: const Center(
+          child: Text(
+            "Add",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
         ),
       ),
     );
