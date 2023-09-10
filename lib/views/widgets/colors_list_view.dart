eecc7684eb9a7de6224694118b75130bbc1ac140
osa989 <osamagamal>
2023-09-10 17:27:46 +0300

    add note bottom sheet done and finish colors

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -53,8 +55,9 @@ class _ColorsListviewState extends State<ColorsListview> {
             child: GestureDetector(
               onTap: () {
                 currentIndex = index;
+                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                 setState(() {});
-              },
+              },ك
               child: ColorItem(
                 color: colors[index],
                 isActive: currentIndex == index,
